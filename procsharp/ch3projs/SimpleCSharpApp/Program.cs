5f35764ec60d076479fd73bed31f07f5dc4e811f
mathdothings <jhonata.prodrigues@gmail.com>
2023-07-01 21:53:35 -0300

    done

--- procsharp/ch3projs/SimpleCSharpApp/Program.cs
+++ procsharp/ch3projs/SimpleCSharpApp/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Display a simple message to the user.
+Console.Title = "My Frist C# App";
+Console.Clear();
+Console.WriteLine("***** My First C# App *****");
