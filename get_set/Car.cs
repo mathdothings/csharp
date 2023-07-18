d02a92379f839328ce2319fe4e80cde12b34aef5
mathdothings <jhonata.prodrigues@gmail.com>
2023-07-17 21:19:12 -0300

    getset learning

---
+++ get_set/Car.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace get_set
+{
+  public class Car
+  {
+    public Car(int maximumVelocity)
+    {
+      MaximumVelocity = maximumVelocity;
+    }
+    int _maximumVelocity;
+
+    public int MaximumVelocity
+    {
+      get { return _maximumVelocity; }
+      set { _maximumVelocity = value; }
+    }
+  }
+}
---
+++ get_set/Program.cs
@@ -0,0 +1,5 @@
+﻿using get_set;
+
+var car1 = new Car(100);
+
+System.Console.WriteLine($"{car1.MaximumVelocity}");
