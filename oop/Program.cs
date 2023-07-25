a5cf2fe70a6fe2abb350a648495ea6cbc0d27661
mathdothings <jhonata.prodrigues@gmail.com>
2023-07-24 23:51:21 -0300

    wip

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace OOP
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var costumer = new Costumer();
+      costumer.Name = "Test";
+      Console.WriteLine(costumer.Name);
+    }
+
+    class Costumer
+    {
+      public string? Name;
+      public Costumer(string? name)
+      {
+        Name = name;
+      }
+
+      public Costumer()
+      {
+
+      }
+    }
+
+    class Payment
+    {
+      public decimal Value { get; set; }
+      public DateTime Expiration { get; set; }
+      public decimal Discount { get; set; }
+      public decimal Fees { get; set; }
+      public decimal Penalty { get; set; }
+
+      void Pay()
+      {
+        ConsultCreditCardBalance();
+      }
+
+      void ConsultCreditCardBalance() { }
+    }
+  }
+}
