87203c3770f86c601428f4e7d08743f29cbe86cf
mathdothings <jhonata.prodrigues@gmail.com>
2023-07-24 21:57:27 -0300

    wip

--- webtest/Program.cs
+++ webtest/Program.cs
@@ -1,6 +1,30 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using App2;
 
-app.MapGet("/", () => "Hello World!");
+namespace App
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      var builder = WebApplication.CreateBuilder(args);
+      var app = builder.Build();
 
-app.Run();
+      var hello = new Program2();
+
+      app.MapGet("/", () => "Hello World!");
+      app.MapGet("/home", () => hello.Show());
+
+      var response = app.MapPost("/home2", () => hello.Show());
+      app.Run();
+
+    }
+  }
+}
+
+namespace App2
+{
+  class Program2
+  {
+    public string Show() { return "Hello!"; }
+  }
+}
