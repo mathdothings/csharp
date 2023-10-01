27c660a8cba22f1280fda816cbecc371c934fecf
mathdothings <jhonata.prodrigues@gmail.com>
2023-10-01 20:26:50 -0300

    practice

--- console/Program.cs
+++ console/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+abstract class Payment
+{
+    public int Id { get; set; }
+    public decimal Value { get; set; }
+    public DateTime Expiration { get; set; }
+    public DateTime DiscountExpiration { get; set; }
+    public DateTime Payday { get; set; }
+    public bool Paid { get; set; }
+    public virtual bool PaymentMethod()
+    {
+        if (Paid) return true;
+        return false;
+    }
+}
