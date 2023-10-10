8cd10e231fa42098f8f5320ce632ff3d82d2aaff
mathdothings <jhonata.prodrigues@gmail.com>
2023-10-09 21:44:37 -0300

    wip

--- console/Program.cs
+++ console/Program.cs
@@ -1,17 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
 
-abstract class Payment
-{
-    public int Id { get; set; }
-    public decimal Value { get; set; }
-    public DateTime Expiration { get; set; }
-    public DateTime DiscountExpiration { get; set; }
-    public DateTime Payday { get; set; }
-    public bool Paid { get; set; }
-    public virtual bool PaymentMethod()
-    {
-        if (Paid) return true;
-        return false;
-    }
-}
+DateOnly thisDate = new DateOnly(2008, 3, 15);
+CultureInfo culture = new CultureInfo("pt-BR");
+Console.WriteLine(thisDate.ToString(CultureInfo.CurrentCulture));
+Console.WriteLine(thisDate.ToString(culture));
+
+System.Console.WriteLine(CultureInfo.CurrentCulture);
